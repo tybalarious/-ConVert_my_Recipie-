2e2d7f01bedefeba99c07c2d3dca7f365fa4c32c
THATONEGUY <tybalarious97@gmail.com>
2016-04-01 08:50:10 -0600

    First commit

---
+++ js/routes.js
@@ -0,0 +1,55 @@
+angular.module('app.routes', [])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+    
+  
+
+      .state('tabsController.home', {
+    url: '/Home',
+    views: {
+      'tab1': {
+        templateUrl: 'templates/home.html',
+        controller: 'homeCtrl'
+      }
+    }
+  })
+
+  .state('tabsController.ingredients', {
+    url: '/Ingredients',
+    views: {
+      'tab2': {
+        templateUrl: 'templates/ingredients.html',
+        controller: 'ingredientsCtrl'
+      }
+    }
+  })
+
+  .state('tabsController', {
+    url: '/page1',
+    templateUrl: 'templates/tabsController.html',
+    abstract:true
+  })
+
+  .state('login', {
+    url: '/page5',
+    templateUrl: 'templates/login.html',
+    controller: 'loginCtrl'
+  })
+
+  .state('signup', {
+    url: '/Signup',
+    templateUrl: 'templates/signup.html',
+    controller: 'signupCtrl'
+  })
+
+$urlRouterProvider.otherwise('/page5')
+
+  
+
+});
