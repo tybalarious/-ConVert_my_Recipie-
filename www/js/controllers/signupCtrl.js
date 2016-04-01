066da649547967098521c525aa6fbf350cf396c8
THATONEGUY <tybalarious97@gmail.com>
2016-04-01 12:12:08 -0600

    Merge branch 'gabeBr' into TylerBr
    # Conflicts:
    #	ionic.project
    #	www/index.html
    #	www/js/controllers.js
    #	www/templates/login.html

---
+++ www/js/controllers/signupCtrl.js
@@ -0,0 +1,35 @@
+/**
+ * Created by gabed on 4/1/16.
+ */
+angular.module('signupCtrl', [])
+    .controller('signupCtrl',signupCtrl);
+function signupCtrl($state){
+    var sc = this;
+    sc.signingUp = signingUp;
+    function signingUp(theirName,theirEmail, theirPass){
+        var ref = new Firebase("https://convertMyRecipe.firebaseio.com");
+        ref.createUser({
+            email    : theirEmail,
+            password : theirPass
+        }, function(error, userData) {
+            if (error) {
+                console.log("Error creating user:", error);
+            } else {
+                console.log("Successfully created user account with uid:", userData.uid);
+                ref.authWithPassword({
+                    email    : theirEmail,
+                    password : theirPass
+                }, function(error, authData) {
+                    if (error) {
+                        console.log("Login Failed!", error);
+                    } else {
+                        console.log("Authenticated successfully with payload:", authData);
+                        $state.go('tabsController.home');
+                    }
+                });
+            }
+        });
+
+    }
+
+}
