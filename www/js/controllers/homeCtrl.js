0e202383c5ed1508553e1ba3cc86d630d2bc4f2c
Gabe Driggs <gabe.driggs@gmail.com>
2016-04-01 20:36:38 -0600

    tyler is a piece of crap

--- www/js/controllers/homeCtrl.js
+++ www/js/controllers/homeCtrl.js
@@ -8,16 +8,27 @@ function homeCtrl($http){
     var hc = this;
     hc.searchItems = searchItems;
     hc.mealData = [];
+    hc.inputs = [];
+    hc.addInputs = addInputs;
+    hc.deleteIngredients = deleteIngredients;
+    function deleteIngredients(){
+        hc.inputs.pop();
+        return hc.inputs;
+    }
+
+    function addInputs(theirIngredients){
+
+        hc.inputs.push(theirIngredients);
+        document.getElementById("ingredientInput").value = '';
+    }
     function searchItems(){
-        $http.get('/api/recipes/').success(function (data) {
-            document.getElementById('searchItems').disabled = 'true';
+        $http.get('/api/recipes/?i='+hc.inputs).success(function (data) {
+            hc.mealData = [];
             for(var i = 0; i < 10; i++){
+
                 hc.mealData.push(data.results[i]);
-
-
             }
             console.log(hc.mealData);
-
              });
     }
 
